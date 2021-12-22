643e9b8ca90b6d3be48c477315ff9e6ec61ef3d5
guga0712 <martins0712@hotmail.com>
2021-12-22 16:30:19 -0300

    added texts and fixed margins

--- counter.js
+++ counter.js
@@ -1,13 +1,13 @@
 const counters = document.querySelectorAll('.counter');
-const speed = 500; // The lower the slower
+const speed = 700; // The lower the slower
 
 counters.forEach(counter => {
 	const updateCount = () => {
-		const target = +counter.getAttribute('data-target'.toLocaleString("en-US"));
+		const target = +counter.getAttribute('data-target'.toLocaleString("pt-BR"));
 		const count = +counter.innerText;
 
 		// Lower inc to slow and higher to slow
-		const inc = target / speed;
+		const inc = parseInt(target) / speed;
 
 		// console.log(inc);
 		// console.log(count);
@@ -15,13 +15,12 @@ counters.forEach(counter => {
 		// Check if target is reached
 		if (count < target) {
 			// Add inc to count and output in counter
-			counter.innerText = count + inc;
+			counter.innerText = count + parseInt(inc);
 			// Call function every ms
 			setTimeout(updateCount, 1);
 		} else {
-			counter.innerText = target.toLocaleString("en-US");
+			counter.innerText = target.toLocaleString("pt-BR");
 		}
 	};
-
 	updateCount();
 });
